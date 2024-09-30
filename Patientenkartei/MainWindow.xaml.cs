84081581cdaa809c4ffacd90e6fec278a38207ef
Sebastian Hans Weissenborn <weissenborn24seb@gmail.com>
2024-09-30 15:33:54 +0200

    Streams und Files

--- Patientenkartei/MainWindow.xaml.cs
+++ Patientenkartei/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.IO;
+using System.Text; // Input/Output
 
 namespace Patientenkartei
 {
@@ -23,6 +12,24 @@ namespace Patientenkartei
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Doppelklick auf den Button, erstellt Event Handler
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+            string content = textBoxContent.Text;
+            string filename = textBoxFileName.Text;
+            string dirPath = @"Users\SWE\Desktop\test\";
+
+            using (FileStream fs = File.Create(filename + ".txt"))
+            {
+
+                byte[] contentConvertedToBytes = Encoding.ASCII.GetBytes(content);
+                fs.Write(contentConvertedToBytes, 0, contentConvertedToBytes.Length);   
+
+            }
+
+            MessageBox.Show("Date wurde angelegt.");
         }
     }
 }
